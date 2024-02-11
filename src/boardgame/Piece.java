db98a8346db0d38b34a98bbb012d94262e26e145
frilii <gualbertofabricio4@gmail.com>
2024-02-11 01:35:06 -0300

    Camada Chess e imprimindo o tabuleiro

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,5 +1,21 @@
 package boardgame;
 
 public class Piece {
+	protected Piece position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
 
+	protected Board getBoard() {
+		return board;
+	}
+
+	public void setBoard(Board board) {
+		this.board = board;
+	}
+	
+	
 }
