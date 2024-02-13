afffa111e31e58145f6bb9730422ecf282d1f4cd
frilii <gualbertofabricio4@gmail.com>
2024-02-13 13:18:25 -0300

    att:commit

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -16,24 +16,26 @@ public class Board {
 	public int getRows() {
 		return rows;
 	}
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-	public int getColumns() {
+		public int getColumns() {
 		return columns;
 	}
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-	
-	public Piece piece(int row, int colum) {
-		return pieces[row][colum];
+	public Piece piece(int row, int column) {
+		if (!positionExists(row, column)) {
+			throw new BoardException("Position not on the board");
+		}
+		return pieces[row][column];
 	}
 	public Piece piece (Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces [position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position) {
+		if (thereISAPiece(position)) {
+			throw new BoardException("There is already a piece on position " + position);
+		}
 		pieces[position.getRow()][position.getColumn()] =piece;
 		piece.position = position;
 	}
@@ -47,6 +49,9 @@ public class Board {
 	}
 	
 	public boolean thereISAPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
 		return 	piece(position) != null;
 	}
 }
